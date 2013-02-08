a0e85809664ee2286660137a3ec9485949bb2f7d
Scott Balentine <sbalentine@yammer-inc.com>
2013-02-08 01:57:59 -0800

    empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Squash::Application.config.secret_token = 'e36dc7ddc074910aabd38adf9aa6df0ea0de261a8e12e436798a97e181db9919037e4b075b7aa2c90f431a0abe9d1405911ea2ca25353bceac49347ae91b22db'
